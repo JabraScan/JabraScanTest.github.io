728a825f7f214d9e1401d8c54830aab56713f3c1
JabraScan <Jabracadabrap@gmail.com>
2025-08-30 12:02:16 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -43,3 +43,33 @@ export function compareCapNumDesc(a, b) {
 
   return 0;
 }
+
+// utils.js
+export function parseFecha(fechaStr) {
+  if (!fechaStr || !/^\d{2}-\d{2}-\d{4}$/.test(fechaStr)) return null;
+  const [dia, mes, año] = fechaStr.split('-').map(Number);
+  const fecha = new Date(año, mes - 1, dia);
+  if (
+    fecha.getFullYear() !== año ||
+    fecha.getMonth() !== mes - 1 ||
+    fecha.getDate() !== dia
+  ) {
+    return null;
+  }
+  return fecha;
+}
+
+export function generarEtiquetaNuevo(fechaInput) {
+  const hoy = new Date();
+  const fecha = new Date(fechaInput);
+  hoy.setHours(0, 0, 0, 0);
+  fecha.setHours(0, 0, 0, 0);
+  const diferenciaDias = Math.floor((hoy - fecha) / (1000 * 60 * 60 * 24));
+  if (diferenciaDias === 0) {
+    return `<span class="tag-capitulo hoy">hoy</span>`;
+  } else if (diferenciaDias > 0 && diferenciaDias <= 7) {
+    return `<span class="tag-capitulo nuevo">nuevo</span>`;
+  } else {
+    return '';
+  }
+}
