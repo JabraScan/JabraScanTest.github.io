77cda61ea46646890c0d2f7bd9815feb8fc6e952
JabraScan <Jabracadabrap@gmail.com>
2025-08-30 13:03:32 +0200

    Update lector.js

--- js/lector.js
+++ js/lector.js
@@ -1,24 +1,15 @@
-import { initLectorPDF } from './lectorpdfmod.js';
-
 export function abrirLectorPDF() {
   fetch('lectorpdf.html')
     .then(r => r.text())
     .then(html => {
       document.querySelector('main').innerHTML = html;
 
-      if (typeof initlectorpdf === "function") {
-        initlectorpdf();
-      } else {
-        const script = document.createElement('script');
-        script.src = 'js/lectorpdfmod.js';
-        script.onload = () => initlectorpdf();
-        document.body.appendChild(script);
-      }
+      // Importar el módulo dinámicamente
+      import('./lectorpdfmod.js')
+        .then(modulo => {
+          modulo.initLectorPDF(); // Llamar la función exportada
+        })
+        .catch(err => console.error('Error cargando lectorpdfmod.js:', err));
     })
     .catch(err => console.error('Error cargando lectorpdf.html:', err));
 }
-
-
-
-
-
